834634e37f176040be9543872334a9b2bf5eccc8
Vincent Zhang <vzhang@purdue.edu>
2015-04-02 19:27:03 -0400

    Read DNT header

--- src/main/java/co/phoenixlab/dn/dnt/Dnt2SqliteReader.java
+++ src/main/java/co/phoenixlab/dn/dnt/Dnt2SqliteReader.java
@@ -24,11 +24,45 @@
 
 package co.phoenixlab.dn.dnt;
 
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.channels.FileChannel;
+import java.nio.channels.SeekableByteChannel;
 import java.nio.file.Path;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.function.DoubleConsumer;
 
 class Dnt2SqliteReader {
+
+    enum DataType {
+        UINT32("UNSIGNED INT"),
+        INT32("INT"),
+        BOOL("BIT"),
+        FLOAT,
+        DOUBLE,
+        STRING("TEXT");
+
+        private DataType() {
+            this.sqlName = this.name();
+        }
+
+        private DataType(String sqlName) {
+            this.sqlName = sqlName;
+        }
+        public final String sqlName;
+
+        @Override
+        public String toString() {
+            return sqlName;
+        }
+    }
+
+    private static final int HEADER_SIZE = 10;
+    private static final int MAGIC_NUMBER = 0x00000000;
 
     private final Path dntFile;
     private final Connection dbConnection;
@@ -38,9 +72,51 @@ class Dnt2SqliteReader {
         this.dbConnection = dbConnection;
     }
 
-    public void read(DoubleConsumer progressListener) {
+    public void read(DoubleConsumer progressListener)
+            throws SQLException, IOException {
+        progressListener.accept(0D);
+        try (RandomAccessFile randomAccessFile = new RandomAccessFile(dntFile.toFile(), "r")) {
+            FileChannel fileChannel = randomAccessFile.getChannel();
+            ByteBuffer buffer = ByteBuffer.allocate(0xFFFF).order(ByteOrder.LITTLE_ENDIAN);
+            ByteBuffer slicedBuffer = buffer.slice().order(ByteOrder.LITTLE_ENDIAN);
+            slicedBuffer.limit(HEADER_SIZE);
+            fillBuffer(fileChannel, slicedBuffer);
+            int magic = slicedBuffer.getInt();
+            if (magic != MAGIC_NUMBER) {
+                throw new InvalidDntException(dntFile,
+                        String.format("Magic number mismatch, expected 0x%08X, got 0x%08X",
+                                MAGIC_NUMBER,
+                                magic));
+            }
+            int numColumns = Short.toUnsignedInt(slicedBuffer.getShort());
+            long rowCount = Integer.toUnsignedLong(slicedBuffer.getInt());
+            slicedBuffer.rewind();
 
+        } catch (EOFException eof) {
+            throw new InvalidDntException(dntFile, "Unexpected EOF");
+        }
+    }
 
+    private int fillBuffer(SeekableByteChannel byteChannel, ByteBuffer buffer) throws IOException {
+        int bytesRead = 0;
+        int read;
+        while ((read = byteChannel.read(buffer)) > 0) {
+            bytesRead += read;
+        }
+        buffer.rewind();
+        if (read == -1) {
+            throw new EOFException();
+        }
+        return bytesRead;
+    }
 
+    class Column {
+        final String name;
+        final DataType dataType;
+
+        public Column(String name, DataType dataType) {
+            this.name = name;
+            this.dataType = dataType;
+        }
     }
 }
